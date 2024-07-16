442aef11cacba59fed72a02fd667d0ca8e738bf8
Dominika Eder <dominikaeder99@gmail.com>
2024-07-16 19:13:43 +0200

    feat: add news api action functionality

--- English_chatbot/actions/actions.py
+++ English_chatbot/actions/actions.py
@@ -1,17 +1,17 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 import requests
+from newsapi import NewsApiClient
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+weather_api_key = os.getenv('WEATHER_API_KEY')
+news_api_key = os.getenv('NEWS_API_KEY')
 
 
 class ActionTellWeather(Action):
@@ -23,13 +23,12 @@ class ActionTellWeather(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        api_key = 'af728153ba2fa1ad794a2e99ef5efd59'
         city = next(tracker.get_latest_entity_values("place"), None)
 
         base_url = "http://api.openweathermap.org/data/2.5/weather"
         params = {
             'q': city,
-            'appid': api_key,
+            'appid': weather_api_key,
             'units': 'metric'
         }
         response = requests.get(base_url, params=params)
@@ -43,4 +42,33 @@ class ActionTellWeather(Action):
 
         return []
 
+
+class ActionTellNews(Action):
+
+    def name(self) -> Text:
+        return "action_tell_news"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        keyword = next(tracker.get_latest_entity_values("keyword"), None)
+        newsapi = NewsApiClient(api_key=news_api_key)
+
+        news = newsapi.get_everything(q=keyword,
+                                              sources='bbc-news,the-verge',
+                                              domains='bbc.co.uk,techcrunch.com',
+                                              from_param='2024-07-13',
+                                              language='en',
+                                              sort_by='relevancy',
+                                              page=2)
+
+        if news:
+            msg = f"You can read the article '{news['articles'][0]['title']}' here: \n{news['articles'][0]['url']}"
+        else:
+            msg = "I don't have any information about this. Try something different."
+
+        dispatcher.utter_message(text=msg)
+
+        return []
 
