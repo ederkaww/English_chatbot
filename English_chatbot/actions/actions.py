5446c49940e9dd4dd8fb4adbe2bbe40e08a3974e
Dominika Eder <dominikaeder99@gmail.com>
2024-07-16 15:12:38 +0200

    add weather api functionality with actions

--- English_chatbot/actions/actions.py
+++ English_chatbot/actions/actions.py
@@ -7,21 +7,40 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+import requests
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionTellWeather(Action):
+
+    def name(self) -> Text:
+        return "action_tell_weather"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        api_key = 'af728153ba2fa1ad794a2e99ef5efd59'
+        city = next(tracker.get_latest_entity_values("place"), None)
+
+        base_url = "http://api.openweathermap.org/data/2.5/weather"
+        params = {
+            'q': city,
+            'appid': api_key,
+            'units': 'metric'
+        }
+        response = requests.get(base_url, params=params)
+        if response.status_code == 200:
+            weather_data = response.json()
+            msg = f"Temperature now in {city}: {weather_data['main']['temp']}°C"
+        else:
+            msg = "I don't have any information about the weather there. Please give me some other city."
+
+        dispatcher.utter_message(text=msg)
+
+        return []
+
+
