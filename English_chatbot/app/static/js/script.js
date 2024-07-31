f92c72691712b848fa2755b3fa46f7e3fb11b032
Dominika Eder <dominikaeder99@gmail.com>
2024-07-31 16:55:47 +0200

    feat: integrate with JG chatbot widget

--- English_chatbot/app/static/js/script.js
+++ English_chatbot/app/static/js/script.js
@@ -1,49 +1,67 @@
-function handleKeyPress(event) {
-    if (event.key === 'Enter') {
-        sendMessage();
-    }
+
+/* module for importing other js files */
+function include(file) {
+  const script = document.createElement('script');
+  script.src = file;
+  script.type = 'text/javascript';
+  script.defer = true;
+
+  document.getElementsByTagName('head').item(0).appendChild(script);
 }
 
-function sendMessage() {
-    const userInput = document.getElementById('user-input').value;
-    if (userInput.trim() === "") return;
 
-    appendMessage(userInput, 'user');
-    document.getElementById('user-input').value = '';
+// Bot pop-up intro
+document.addEventListener("DOMContentLoaded", () => {
+  const elemsTap = document.querySelector(".tap-target");
+  // eslint-disable-next-line no-undef
+  const instancesTap = M.TapTarget.init(elemsTap, {});
+  instancesTap.open();
+  setTimeout(() => {
+    instancesTap.close();
+  }, 4000);
+});
 
-    fetch('http://localhost:5000/webhook', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ message: userInput }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.response) {
-            appendMessage(data.response, 'bot', true);
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
+/* import components */
+include('./static/js/components/index.js');
+
+window.addEventListener('load', () => {
+  // initialization
+  $(document).ready(() => {
+    // Bot pop-up intro
+    $("div").removeClass("tap-target-origin");
+
+    // drop down menu for close, restart conversation & clear the chats.
+    $(".dropdown-trigger").dropdown();
+
+    // initiate the modal for displaying the charts,
+    // if you dont have charts, then you comment the below line
+    $(".modal").modal();
+
+    // enable this if u have configured the bot to start the conversation.
+    // showBotTyping();
+    // $("#userInput").prop('disabled', true);
+
+    // if you want the bot to start the conversation
+    // customActionTrigger();
+  });
+  // Toggle the chatbot screen
+  $("#profile_div").click(() => {
+    $(".profile_div").toggle();
+    $(".widget").toggle();
+  });
+
+  // clear function to clear the chat contents of the widget.
+  $("#clear").click(() => {
+    $(".chats").fadeOut("normal", () => {
+      $(".chats").html("");
+      $(".chats").fadeIn();
     });
-}
+  });
 
-function appendMessage(message, sender, isHtml = false) {
-    const messageElement = document.createElement('div');
-    messageElement.classList.add('message', sender);
-
-    const messageContent = document.createElement('div');
-    messageContent.classList.add('message-content');
-    if (isHtml) {
-        messageContent.innerHTML = message;  // Interpret HTML
-    } else {
-        messageContent.textContent = message;  // Render as text
-    }
-
-    messageElement.appendChild(messageContent);
-
-    const chatBox = document.getElementById('chat-box');
-    chatBox.appendChild(messageElement);
-    chatBox.scrollTop = chatBox.scrollHeight;
-}
+  // close function to close the widget.
+  $("#close").click(() => {
+    $(".profile_div").toggle();
+    $(".widget").toggle();
+    scrollToBottomOfResults();
+  });
+});
